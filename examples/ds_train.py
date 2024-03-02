db906b653a0fe17bcfe97dddb22dac8eaff8bd14
vital121 <123862035+vital121@users.noreply.github.com>
2024-03-02 15:52:26 +0000

    Update ds_train.py

--- examples/ds_train.py
+++ examples/ds_train.py
@@ -1,3 +1,39 @@
+"""
+This script fine-tunes a pre-trained causal language model (CLM) using the Hugging Face Transformers library on a specified dataset.
+It supports injecting custom linear layers (`BitLinear` or `BitLinear158`) into the model architecture, which can be useful for
+experimental modifications or optimizations. Additionally, it provides functionality for data loading, tokenization,
+training argument configuration, and custom callbacks for monitoring training progress.
+
+Usage:
+    Run this script from the command line with the necessary arguments. For example:
+    ```
+    python script_name.py --model_id microsoft/phi-2 --dataset EleutherAI/wikitext_document_level --subset wikitext-103-raw-v1 --output_dir saved_model --inject BitLinear
+    ```
+
+Arguments:
+    --model_id (str): The ID of the pre-trained model to fine-tune. Defaults to "microsoft/phi-2".
+    --dataset (str): The dataset to use for fine-tuning. Defaults to "EleutherAI/wikitext_document_level".
+    --subset (str): The specific subset of the dataset to use. Defaults to "wikitext-103-raw-v1".
+    --output_dir (str): Directory where the fine-tuned model and checkpoints will be saved. Defaults to "saved_model".
+    --inject (str): Specifies the type of custom linear layer to inject into the model. Choices are "BitLinear158", "BitLinear", and "None". Defaults to "BitLinear".
+
+DeepSpeed Arguments:
+    --train_args_file (str): Optional file containing training arguments for advanced configurations.
+    --deepspeed (str): Optional DeepSpeed configuration file for leveraging DeepSpeed optimizations.
+    --local_rank (int): Local rank passed from distributed launcher. Default is -1, indicating non-distributed training.
+
+Functionality:
+    1. Loads the specified pre-trained causal language model and tokenizer.
+    2. Optionally modifies the model by replacing `nn.Linear` layers with custom `BitLinear` or `BitLinear158` layers.
+    3. Loads and tokenizes the specified dataset, preparing it for training.
+    4. Configures training arguments, including DeepSpeed integration if specified.
+    5. Initializes a `Trainer` instance with the model, data, tokenizer, and training arguments.
+    6. Starts the training process with an optional custom callback for monitoring first layer gradients.
+
+Custom Callbacks:
+    - PrintFirstLayerGradientsCallback: A callback that prints the gradients of the first layer of the model at the end of each training step.
+"""
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
